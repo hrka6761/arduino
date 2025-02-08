9efb183dd14587e19977ecc637281fd47664cf33
Hamidreza Karami <h.karami@dariahamrah.ir>
2025-02-08 18:41:36 +0330

    temp

--- app/src/main/java/ir/hrka/arduino/core/Constants.kt
+++ app/src/main/java/ir/hrka/arduino/core/Constants.kt
@@ -3,4 +3,5 @@ package ir.hrka.arduino.core
 object Constants {
 
     const val ACTION_USB_PERMISSION = "ir.hrka.arduino.USB_PERMISSION"
+    const val USB_PERMISSION_REQUEST_CODE = 1001
 }
--- app/src/main/java/ir/hrka/arduino/ui/screens/MainScreen.kt
+++ app/src/main/java/ir/hrka/arduino/ui/screens/MainScreen.kt
@@ -1,5 +1,11 @@
 package ir.hrka.arduino.ui.screens
 
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
+import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,11 +17,13 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,11 +31,17 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ir.hrka.arduino.MainActivity
 import ir.hrka.arduino.R
+import ir.hrka.arduino.core.Constants.ACTION_USB_PERMISSION
+import ir.hrka.arduino.core.Constants.USB_PERMISSION_REQUEST_CODE
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
+    val activity = LocalActivity.current as MainActivity
     val snackBarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
 
     Scaffold(
         modifier = modifier.fillMaxSize(),
@@ -47,6 +61,25 @@ fun MainScreen(modifier: Modifier = Modifier) {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            var usbDevice: UsbDevice? = null
+            val usbManager = activity.getSystemService(Context.USB_SERVICE) as UsbManager
+            val permissionIntent = PendingIntent
+                .getBroadcast(
+                    activity,
+                    USB_PERMISSION_REQUEST_CODE,
+                    Intent(ACTION_USB_PERMISSION),
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+
+            val deviceList = usbManager.deviceList
+            if (deviceList.isNotEmpty()) {
+                usbDevice = deviceList.values.firstOrNull()
+                usbDevice?.let { usbManager.requestPermission(usbDevice, permissionIntent) }
+            } else {
+
+            }
+
+
             Button(
                 modifier = modifier
                     .size(80.dp)
